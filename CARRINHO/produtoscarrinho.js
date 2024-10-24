370261fd929076e2508bea4d1e7eaf56f0f2d4de
sauerdev <davidemanuelsauer@gmail.com>
2024-10-24 16:32:40 -0300

    24/10 ATUALIZAÃO FOOTER, HEADER, E CARTOES

--- CARRINHO/produtoscarrinho.js
+++ CARRINHO/produtoscarrinho.js
@@ -3,8 +3,8 @@ const products = [
         title: "Produto 1",
         description: "Descrição breve do produto 1.",
         images: [
-            "https://via.placeholder.com/150/FF5733", // Imagem 1
-            "https://via.placeholder.com/150/C70039"  // Imagem 2
+            "https://imgcentauro-a.akamaihd.net/1366x1366/M0V6BY11.jpg", // Imagem 1
+            "https://cdn.sistemawbuy.com.br/arquivos/13ef42c661356c967c0cee1859873499/produtos/65a9930f70caf/preta-costas-65de22e61e597.jpg"  // Imagem 2
         ],
     },
     {
@@ -71,7 +71,62 @@ const products = [
             "https://via.placeholder.com/150/900C3F", // Imagem 2
             "https://via.placeholder.com/150/FFC300"  // Imagem 3 
         ],
-    }
+    },
+
+    {
+        title: "Produto 9",
+        description: "Descrição breve do produto 9.",
+        images: [
+            "https://via.placeholder.com/150/581845", // Imagem 1
+            "https://via.placeholder.com/150/900C3F", // Imagem 2
+            "https://via.placeholder.com/150/FFC300"  // Imagem 3 
+        ],
+    },
+    {
+        title: "Produto 9",
+        description: "Descrição breve do produto 9.",
+        images: [
+            "https://via.placeholder.com/150/581845", // Imagem 1
+            "https://via.placeholder.com/150/900C3F", // Imagem 2
+            "https://via.placeholder.com/150/FFC300"  // Imagem 3 
+        ],
+    },
+    {
+        title: "Produto 9",
+        description: "Descrição breve do produto 9.",
+        images: [
+            "https://via.placeholder.com/150/581845", // Imagem 1
+            "https://via.placeholder.com/150/900C3F", // Imagem 2
+            "https://via.placeholder.com/150/FFC300"  // Imagem 3 
+        ],
+    },
+    {
+        title: "Produto 9",
+        description: "Descrição breve do produto 9.",
+        images: [
+            "https://via.placeholder.com/150/581845", // Imagem 1
+            "https://via.placeholder.com/150/900C3F", // Imagem 2
+            "https://via.placeholder.com/150/FFC300"  // Imagem 3 
+        ],
+    },
+    {
+        title: "Produto 9",
+        description: "Descrição breve do produto 9.",
+        images: [
+            "https://via.placeholder.com/150/581845", // Imagem 1
+            "https://via.placeholder.com/150/900C3F", // Imagem 2
+            "https://via.placeholder.com/150/FFC300"  // Imagem 3 
+        ],
+    },
+    {
+        title: "Produto 9",
+        description: "Descrição breve do produto 9.",
+        images: [
+            "https://via.placeholder.com/150/581845", // Imagem 1
+            "https://via.placeholder.com/150/900C3F", // Imagem 2
+            "https://via.placeholder.com/150/FFC300"  // Imagem 3 
+        ],
+    },
 ];
 
 function initializeProducts() {
