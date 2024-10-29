4f3f7878a12d942b0b53a72059e21308a9246c7b
Pssat <juan_pagnussat@outlook.com>
2024-10-29 14:14:37 -0300

    .

--- CARRINHO/funcionalidades.js
+++ CARRINHO/funcionalidades.js
@@ -1,13 +1,13 @@
-function toggleMapPanel() {
-    const mapPanel = document.querySelector('.map-panel');
-    const arrow = document.querySelector('.arrow');
+// function toggleMapPanel() {
+//     const mapPanel = document.querySelector('.map-panel');
+//     const arrow = document.querySelector('.arrow');
 
-    // Alterna a posição do painel e da seta
-    if (mapPanel.style.right === '0px') {
-        mapPanel.style.right = '-25%';
-        arrow.style.right = '0';
-    } else {
-        mapPanel.style.right = '0';
-        arrow.style.right = '25%';
-    }
-}
+//     // Alterna a posição do painel e da seta
+//     if (mapPanel.style.right === '0px') {
+//         mapPanel.style.right = '-25%';
+//         arrow.style.right = '0';
+//     } else {
+//         mapPanel.style.right = '0';
+//         arrow.style.right = '25%';
+//     }
+// }
