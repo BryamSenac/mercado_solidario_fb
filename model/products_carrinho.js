39cadd89feaf6769d0167a7184bcd63eca52a37b
sauerdev <davidemanuelsauer@gmail.com>
2024-11-08 13:57:09 -0300

    07/11 att carrinho na main

--- model/products_carrinho.js
+++ model/products_carrinho.js
@@ -1,4 +1,4 @@
-export const products_carrinho = [
+export const products_carrinho= [
     {
         title: "Produto 1",
         description: "Descrição breve do produto 1.",
@@ -11,65 +11,65 @@ export const products_carrinho = [
         title: "Produto 2",
         description: "Descrição breve do produto 2.",
         images: [
-            "https://via.placeholder.com/150/FFC300", // Imagem 1
-            "https://via.placeholder.com/150/DAF7A6"  // Imagem 2
+            "https://images.tcdn.com.br/img/img_prod/698738/copia_jaqueta_masculina_puffer_nylon_preta_747_1_378b31a1d36e3a94d141494c066d7f39.jpg", // Imagem 1
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQDzFAqLhxMTBX6_5Urpjptbry40HGCTcJVoPFZ83sXjm8CPTqVs-UaDQFZxkEQzWTMMVE&usqp=CAU"  // Imagem 2
         ],
     },
     {
         title: "Produto 3",
         description: "Descrição breve do produto 3.",
         images: [
-            "https://via.placeholder.com/150/581845", // Imagem 1
-            "https://via.placeholder.com/150/900C3F"  // Imagem 2
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRcW42Kbm92bsIdSaN7VYu5EbzFxWMJUY5rAw&s", // Imagem 1
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS5g2pHhx5IBX17lrp0dy5ZaVzGOLBLAIXeb5lBP-XHdnTZ2R5Yf2wyJ8iwxsk2Z-hxYEQ&usqp=CAU"  // Imagem 2
         ],
     },
     {
         title: "Produto 4",
         description: "Descrição breve do produto 4.",
         images: [
-            "https://via.placeholder.com/150/FF5733", // Imagem 1
-            "https://via.placeholder.com/150/C70039"  // Imagem 2
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSpp9UwUwitSz_HoNRneF0gb94ADUL8iOdcHQ&s", // Imagem 1
+            "https://images.tcdn.com.br/img/img_prod/1144741/fantasia_t_rex_inflavel_365_1_844f0e7fc7ea7521214e6451419e9a76.jpg"  // Imagem 2
         ],
     },
     {
         title: "Produto 5",
         description: "Descrição breve do produto 5.",
         images: [
-            "https://via.placeholder.com/150/FFC300", // Imagem 1
-            "https://via.placeholder.com/150/DAF7A6"  // Imagem 2
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRSEZDy1nxoaOUASzQ9Psa7DlQ3_m05YEJqgg&s", // Imagem 1
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcREl9CnOPyKHP3-VpXOr1J5i2YwnW6YghcjGTnBzIQFdUAsdwsk9PoGCiLMLq_UGmVbcak&usqp=CAU"  // Imagem 2
         ],
     },
     {
         title: "Produto 6",
         description: "Descrição breve do produto 6.",
         images: [
-            "https://via.placeholder.com/150/581845", // Imagem 1
-            "https://via.placeholder.com/150/900C3F"  // Imagem 2
+            "https://cdnv2.moovin.com.br/maryjane/imagens/produtos/det/tenis-m99-feminino-6a8b3592e4da5c765c122c6ca63e6380.jpg", // Imagem 1
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTyXSAXADK1GOqOGfhE2aK5K9emAnP4m180yYZ0K3nW79ymu1yRLBFU-3HZRsoY43qMgTQ&usqp=CAU"  // Imagem 2
         ],
     },
     {
         title: "Produto 7",
         description: "Descrição breve do produto 7.",
         images: [
-            "https://via.placeholder.com/150/FF5733", // Imagem 1
-            "https://via.placeholder.com/150/C70039"  // Imagem 2
+            "https://http2.mlstatic.com/D_NQ_NP_869002-MLB69239790002_052023-O.webp", // Imagem 1
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQkGzCWGE_mp5fzxLEjWQmyZA8JALj7_s-8OAm2qXpGB59FpDrVu_MpDjxa-s4D6I--LLM&usqp=CAU"  // Imagem 2
         ],
     },
     {
         title: "Produto 8",
         description: "Descrição breve do produto 8.",
         images: [
-            "https://via.placeholder.com/150/FFC300", // Imagem 1
-            "https://via.placeholder.com/150/DAF7A6"  // Imagem 2
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRuwsgzj-lhVFrSPIG2jxpnIN6sIJ_VvtvBDg&s", // Imagem 1
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSHnL6fqqaMMHxY2kwC3aXsraFcI2k43f1orSji9hibPzd5UYHHVGJqhCmBzR8-GB0_GoU&usqp=CAU"  // Imagem 2
         ],
     },
     {
         title: "Produto 9",
         description: "Descrição breve do produto 9.",
         images: [
-            "https://via.placeholder.com/150/581845", // Imagem 1
-            "https://via.placeholder.com/150/900C3F", // Imagem 2
-            "https://via.placeholder.com/150/FFC300"  // Imagem 3 
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_CK4CTUR7BE-LeW8HSaW8M-CXb5n56bo4AA&s", // Imagem 1
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRIwIddwIv8tPcEVmubtPPfsiuzpsW-4zTPwhFeiT7n8Zc25-z87BY_sBaWEGnoKu7mPgE&usqp=CAU", // Imagem 2
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRpUdPKCSeTJzNQltAQU01PmpR06_9uMtFp90_MlVGgjnB_LRzifrkw7XaMhZGKs8-_DCw&usqp=CAU"  // Imagem 3 
         ],
     },
 
@@ -77,54 +77,54 @@ export const products_carrinho = [
         title: "Produto 9",
         description: "Descrição breve do produto 9.",
         images: [
-            "https://via.placeholder.com/150/581845", // Imagem 1
-            "https://via.placeholder.com/150/900C3F", // Imagem 2
-            "https://via.placeholder.com/150/FFC300"  // Imagem 3 
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTjads1oUhcrYpIGfJiVJSfqY97onJXfOVxnQ&s", // Imagem 1
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRE2_2g1Mh5KM-pfnSoQw_BzVEsgZM7rLRftaii7arXxKsMD1Eo2XIPWmbNdn86M7UppuY&usqp=CAU", // Imagem 2
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS5ucH72rCeWNbxXl7Uiv5w4kQXPY_7IaB3RXCJ9Qffc9osJmPS7np4ziGRfQUsodSH1Bk&usqp=CAU"  // Imagem 3 
         ],
     },
     {
         title: "Produto 9",
         description: "Descrição breve do produto 9.",
         images: [
-            "https://via.placeholder.com/150/581845", // Imagem 1
-            "https://via.placeholder.com/150/900C3F", // Imagem 2
-            "https://via.placeholder.com/150/FFC300"  // Imagem 3 
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT9CbXWmRvofx0GvShkXCaRHZCN4JOixKMkng&s", // Imagem 1
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcShJ5FoGHf9d7Jw1IbH4h2ay9LeqgwwMtBmsGK_sHBajgFa0JCcHRftwI-e3pz_-X4HCfg&usqp=CAU", // Imagem 2
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSdEldSxJgZ59WIScGDtLAc1clJiXKM8pmPpyeEli9lWxNUxuwFipQj201QW7dMKCnpXiI&usqp=CAU"  // Imagem 3 
         ],
     },
     {
         title: "Produto 9",
         description: "Descrição breve do produto 9.",
         images: [
-            "https://via.placeholder.com/150/581845", // Imagem 1
-            "https://via.placeholder.com/150/900C3F", // Imagem 2
-            "https://via.placeholder.com/150/FFC300"  // Imagem 3 
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSSoBfrgkht63sb54ea8MxnUb-nou_GLB_bzsjs7dSlAc4-WVvGXC4KLUrZVZNoiGYSxPs&usqp=CAU", // Imagem 1
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS38Q3jbAZ9y_2JezIRpojBVfm0sWxF8Ga_sULnEid7Y9j1rBt7lKKZvLEFmSpiJ2v6g6k&usqp=CAU   ", // Imagem 2
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRfKaZvdm5Ky5jm47I7IMDpGD0F-XJpbh1_hnEnorGTiAwE9BzdPQO3nNuZ0ob9nor3aOk&usqp=CAU"  // Imagem 3 
         ],
     },
     {
         title: "Produto 9",
         description: "Descrição breve do produto 9.",
         images: [
-            "https://via.placeholder.com/150/581845", // Imagem 1
-            "https://via.placeholder.com/150/900C3F", // Imagem 2
-            "https://via.placeholder.com/150/FFC300"  // Imagem 3 
+            "https://a-static.mlcdn.com.br/800x560/jogo-lencol-fronha-infantil-pica-pau-impakto-visual/impaktovisual/lenfronpicapau/f24b550d8b0d2eff8c31a3e2fecad4b2.jpeg", // Imagem 1
+            "https://down-br.img.susercontent.com/file/br-11134201-7r98o-lmzwui7q6epne1", // Imagem 2
+            "https://http2.mlstatic.com/D_605508-MLB54226567761_032023-C.jpg"  // Imagem 3 
         ],
     },
     {
         title: "Produto 9",
         description: "Descrição breve do produto 9.",
         images: [
-            "https://via.placeholder.com/150/581845", // Imagem 1
-            "https://via.placeholder.com/150/900C3F", // Imagem 2
-            "https://via.placeholder.com/150/FFC300"  // Imagem 3 
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTMfNleryAxVTx8_ygjJcqAUzKdQkbIHL0dzjn1VbsOxgmXiqAyMaiRRy4Ls9HIroTmVcU&usqp=CAU", // Imagem 1
+            "https://montink.s3.amazonaws.com/mockups/194247/Branco_1227322.png", // Imagem 2
+            "https://montink.s3.amazonaws.com/mockups/194247/Preto_1227322.png"  // Imagem 3 
         ],
     },
     {
         title: "Produto 9",
         description: "Descrição breve do produto 9.",
         images: [
-            "https://via.placeholder.com/150/581845", // Imagem 1
-            "https://via.placeholder.com/150/900C3F", // Imagem 2
-            "https://via.placeholder.com/150/FFC300"  // Imagem 3 
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTubGoNLm0cHkr-ZKYOcagsNxUpWgw9iJeMqQ&s", // Imagem 1
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQLzT0M_KhBWrT60xRtXHOSog1lt_Ibfsq9glV05kfVNb5nE40v5hE3vKVrsu5O5VXHpr0&usqp=CAU", // Imagem 2
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSu4RGXCFMvDK8_5XOM4MUvbmiqbM4667I16Spb_Sugi7ZcrtsY8EWPg6q5IYbv--xLYK4&usqp=CAU"  // Imagem 3 
         ],
     },
 ];
