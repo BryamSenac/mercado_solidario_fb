460f514044ad6793c0789de112f38c18a0b2de56
Pssat <juan_pagnussat@outlook.com>
2024-11-11 11:08:51 -0300

    Arrumei os cartões
    Apenas mantive um padrão para os cartões, pois o btn contato estava distoando de cartão para cartão

--- model/destaques.js
+++ model/destaques.js
@@ -1,33 +1,33 @@
 export const destaques = [
     {
         title: "Campanha do agasalho",
-        description: "Campanha de arrecadação de agasalhos para pessoas em situação de vulnerabilidade.",
+        description: "Arrecadação de agasalhos .",
         images: [
-            "https://via.placeholder.com/150/008000", // Imagem 1
+            "https://www.camaraquatrobarras.pr.gov.br/site2019/wp-content/uploads/CAMPANHA-DO-AGASALHO-1-2.jpg", // Imagem 1
             "https://via.placeholder.com/150/0000FF"  // Imagem 2
         ],
     },
     {
         title: "Doação de livros",
-        description: "Campanha para doação de livros para bibliotecas comunitárias e escolas.",
+        description: "Campanha para doação de livros .",
         images: [
-            "https://via.placeholder.com/150/FF6347", // Imagem 1
+            "https://portal.fslf.edu.br/wp-content/uploads/2022/02/doe_um_livro.jpg", // Imagem 1
             "https://via.placeholder.com/150/4682B4"  // Imagem 2
         ],
     },
     {
         title: "SOS Rio Grande do Sul",
-        description: "Ajuda para as vítimas de desastres naturais no Rio Grande do Sul.",
+        description: "Ajude no Rio Grande do Sul.",
         images: [
-            "https://via.placeholder.com/150/FFA500", // Imagem 1
+            "https://img.fae.edu/galeria/getImage/362/5130104515343779.png", // Imagem 1
             "https://via.placeholder.com/150/DA70D6"  // Imagem 2
         ],
     },
     {
         title: "Brinquedos Do Bem",
-        description: "Campanha de doação de brinquedos para crianças em situação de vulnerabilidade.",
+        description: "Doação de brinquedos.",
         images: [
-            "https://via.placeholder.com/150/40E0D0", // Imagem 1
+            "https://www.otaboanense.com.br/wp-content/uploads/2019/09/doe_brinquedos_shopping_tab.jpg", // Imagem 1
             "https://via.placeholder.com/150/9370DB"  // Imagem 2
         ],
     },
--- model/products.js
+++ model/products.js
@@ -1,21 +1,21 @@
 export const products = [
     {
         title: "Fone de Ouvido Bluetooth",
-        description: "Fone de ouvido sem fio com cancelamento de ruído.",
+        description: "Fone de ouvido sem fio.",
         images: [
             "https://images-americanas.b2w.io/produtos/7476173185/imagens/fone-ouvido-bluetooth-sem-fio-estereo-alta-potencia-case-de-recarga-fone-compativel-iphone-samsung-motorola-xiaomi/7476173185_1_large.jpg", // Imagem 1
         ],
     },
     {
         title: "Relógio Inteligente",
-        description: "Relógio inteligente com monitor de frequência cardíaca.",
+        description: "possui monitor de frequência cardíaca.",
         images: [
             "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRSH8SIlxZjt6CN_bsVwzaLt1JJB44C1ZbiHA&s", // Imagem 1
         ],
     },
     {
         title: "Jaqueta de Couro",
-        description: "Jaqueta de couro sintético, estilo urbano.",
+        description: "Jaqueta de couro sintético.",
         images: [
             "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQDWT7K7HaasH42s1FiDh54ck2zX5CHba7ekbG0J3PHvKaJy5fT6gL7Gl_djnWkeMfyh7U&usqp=CAU", // Imagem 
         ],
@@ -29,35 +29,35 @@ export const products = [
     },
     {
         title: "Cafeteira Elétrica",
-        description: "Cafeteira automática para café expresso.",
+        description: "Cafeteira automática.",
         images: [
             "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSY4YDuz5UW_y7ZoAjpvL_YiYyjPWrp2YgvWQ&s", // Imagem 1
         ],
     },
     {
         title: "Garrafa Térmica",
-        description: "Garrafa térmica em aço inox, 500 ml.",
+        description: "Garrafa térmica em aço inox.",
         images: [
             "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRsMiMS73eGuwd8jMSm4eq16SrJHiJ2SLF2Lg&s", // Imagem 1
         ],
     },
     {
         title: "Cadeira Gamer",
-        description: "Cadeira ergonômica para jogos e escritório.",
+        description: "Cadeira ergonômica gamer.",
         images: [
             "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcStDyGThOFlt8Seq5tOP0e55sY3qjbL_n2Zqg&s", // Imagem 1
         ],
     },
     {
         title: "Mesa de Escritório",
-        description: "Mesa de escritório em MDF com gavetas.",
+        description: "Mesa de escritório em MDF.",
         images: [
             "https://images.tcdn.com.br/img/img_prod/943090/mesa_escritorio_grande_3_gav_160cm_combinare_noce_preto_3077_1_9aa74f3bc344c035e4fd1210e6627968.jpg", // Imagem 1
         ],
     },
     {
         title: "Conjunto de Panelas",
-        description: "Conjunto de panelas antiaderentes de alumínio.",
+        description: "Conjunto de panelas antiaderentes.",
         images: [
             "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR1flAFgjTtGtySmffzbxinlCWlGOmuKKLJIQ&s", // Imagem 1
         ],
